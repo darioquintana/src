d55ff26f4078c8cbf5da0cd06f66b1405ceb8281
Dario Quintana <contacto@darioquintana.com.ar>
2012-04-30 03:59:44 -0300

    ACS web sample

---
+++ Azure/HelloAllOfYouQu/HelloAllOfYouQu/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+using Microsoft.IdentityModel.Claims;
+using System.Linq;
+namespace HelloAllOfYouQu
+{
+	public partial class Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			var claims = User.Identity as ClaimsIdentity;
+			
+			claims
+				.Claims
+				.ToList()
+				.ForEach(c => Response.Write("<br>Type: " + c.ClaimType + " ; Value: " + c.Value));
+		}
+	}
+}
